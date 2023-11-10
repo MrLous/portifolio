60bf29267db1007ca324218023b8c0558ee4bef9
Juh <ricardoluizjr@live.com>
2023-11-10 17:12:13 -0300

    modified:   src/components/section/Presentation.js 	modified:   src/components/section/Skills.js 	new file:   src/components/section/Skills.module.css 	new file:   src/image/skills/css.png 	new file:   src/image/skills/css.svg 	new file:   src/image/skills/html.png 	new file:   src/image/skills/html.svg 	new file:   src/image/skills/inkscape.png 	new file:   src/image/skills/inkscape.svg 	new file:   src/image/skills/javascript.png 	new file:   src/image/skills/javascript.svg 	new file:   src/image/skills/power-bi.png 	new file:   src/image/skills/power-bi.svg 	new file:   src/image/skills/react.png 	new file:   src/image/skills/react.svg

--- src/components/section/Skills.js
+++ src/components/section/Skills.js
@@ -1,7 +1,26 @@
+
+import styles from  './Skills.module.css'
+import javascriptImg from '../../image/skills/javascript.png'
+import htmlImg from '../../image/skills/html.png'
+import cssImg from '../../image/skills/css.png'
+import reactImg from '../../image/skills/react.png'
+import powerbiImg from '../../image/skills/power-bi.png'
+import inkscapeImg from '../../image/skills/inkscape.png'
+
+
 function Skills(){
     return(
-      <div id='Skill'>
-        <h1> Aqui é a Skills</h1>
+      <div className={styles.skill} id='Skill'>
+        <h1> Habilidades</h1>
+        <p>Conheça um pouco das minhas principais habilidades e conhecimentos </p>
+        <div>
+          <img src={inkscapeImg} />
+          <img src={javascriptImg} />
+          <img src={htmlImg} />
+          <img src={cssImg} />
+          <img src={reactImg} />
+          <img src={powerbiImg} />
+        </div>
       </div>  
     )
 }
