965701f63ad565698c41a08992efb5e769f100f3
Juh <ricardoluizjr@live.com>
2023-11-14 17:09:37 -0300

    modified:   src/components/elements/Cards.js 	modified:   src/components/elements/Cards.module.css 	modified:   src/components/section/Projects.js 	modified:   src/components/section/Skills.module.css 	renamed:    src/image/projects/Interface.PNG -> src/image/projects/danplas.PNG

--- src/components/elements/Cards.js
+++ src/components/elements/Cards.js
@@ -1,9 +1,24 @@
-function Cards(){
+import styles from '../elements/Cards.module.css'
+import ButtonB from '../elements/ButtonB'
+
+
+
+function Cards({img, title, tech, descripion, repo, site}){
     return (
-        <div>
-            Projetos...github 
+        <div className={styles.cards}>
+            <a href={site} target='_blank'>
+                <img src={img}/>
+            </a>
+            <section>
+                <h3>{title}</h3>
+                <p>{tech}</p>
+                <p>{descripion}</p>
+                <ButtonB text="Acesse me repositório" link={repo} target='_blank'/>
+            </section>
         </div>
     )
 }
 
 export default Cards
+
+
--- src/components/section/Projects.js
+++ src/components/section/Projects.js
@@ -1,19 +1,48 @@
 import styles from '../section/Projects.module.css'
-import ButtonB from '../elements/ButtonB'
-import Cards from '../elements/cards'
+import Cards from '../elements/Cards'
+import crud from '../../image/projects/CRUF.PNG'
+import dnc from '../../image/projects/DNC.PNG'
+import danplas from '../../image/projects/danplas.PNG'
+import javascriptImg from '../../image/skills/javascript.png'
+import htmlImg from '../../image/skills/html.png'
+import cssImg from '../../image/skills/css.png'
+import reactImg from '../../image/skills/react.png'
+import powerbiImg from '../../image/skills/power-bi.png'
+import inkscapeImg from '../../image/skills/inkscape.png'
 
 function Projects(){
     return(
       <div id='Projects' className={styles.projects}>
         <h1>Projetos</h1>
-        <Cards/>
-        <ButtonB text="Acesse me repositório" link='https://github.com/MrLous/crud-task' target='_blank'/>
-        <Cards/>
-        <ButtonB text="Acesse me repositório" link='https://github.com/MrLous/terminal' target='_blank'/>
-        <Cards/>
-        <ButtonB text="Acesse me repositório" link='https://github.com/MrLous/ProjetoDNC01' target='_blank'/>
+        <Cards
+        img={crud}
+        title = "C.R.U.D"
+        tech = "PHP, JS, HTML, CSS e iks"
+        descripion = "Gerenciador de tarefas simples com controle de status e dinÂmico"
+        repo = 'https://github.com/MrLous/crud-task'
+        site = " "
+        />
+        <Cards
+        img={dnc}
+        title = "Leading Page"
+        tech = "HTML e CSS"
+        descripion = "Desenvolvimento de uma Leading Page para formação pela escola DNC "
+        repo = 'https://github.com/MrLous/ProjetoDNC01'
+        site = 'https://projetolearningdnc.netlify.app/'
+        />
+        <Cards
+        img={danplas}
+        title = "Terminal de Retirada de Itens"
+        tech = "HTML, JS, CSS e iks"
+        descripion = "Ferramenta desenvolvida para automatizar a Saida de Materiais do almoxarifado."
+        repo = 'https://github.com/MrLous/terminal'
+        site = "https://sprightly-bienenstitch-4aaaf2.netlify.app"
+        />
       </div>  
     )
 }
+
+
+
 
 export default Projects
