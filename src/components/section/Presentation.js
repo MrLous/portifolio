979d232d75183efa12b5e91a81d60eec4bdfa957
Juh <ricardoluizjr@live.com>
2023-11-16 10:26:12 -0300

    modified:   src/components/section/Presentation.js

--- src/components/section/Presentation.js
+++ src/components/section/Presentation.js
@@ -8,8 +8,8 @@ function Presentation(){
         <h1> Olá, me chamo Ricardo Luiz.</h1>
         <h6> mas pode me chamar de Juh</h6>
         <p>Sou universitário, cursando Engenharia da Computação pela Univesp<br/>
-        Comecei a trabalhar ainda como menor aprendiz na Industria metalúrgica, me tornei mais organizado e passei a planejar as tarefas consegui de melhor forma, garantindo meu lugar e passei de menor aprendiz para, auxiliar, estoquista e por fim coordenador de estoque e comprador.<br/>
-        Construí minha carreira melhorando, desenvolvendo ferramentas para acelerar e facilitar o processo como também o produto através estudos, experiência e sim, erros. Aplicando o conhecimento, idealizei algumas ferramentais em Excel, Power-bi e AppWEB's.<br/>
+        Comecei a trabalhar ainda como menor aprendiz na Industria metalúrgica, me tornei mais organizado e passei a planejar as tarefas conseguindo melhorar meus projetos e otimizando as entregas, garantindo meu lugar. Passei de menor aprendiz para auxiliar, estoquista, coordenador de estoque e comprador.<br/>
+        Construí minha carreira melhorando e desenvolvendo, aplicativos e ferramentas para acelerar o processo fazendo uma gestão mais fiel com rápidas tomadas de decisões, essa evolução se deu através estudos, experiência e sim, erros.<br/>
         <br/>Atualmente atuo como designer e desenvolvedor autônomo, mas espero embreve estar Trabalhando com você</p>
         <ButtonA text='Conecte-se comigo!' link='https://www.linkedin.com/in/ricardoluizjr/'  target='_blank'/>
       </div> 
