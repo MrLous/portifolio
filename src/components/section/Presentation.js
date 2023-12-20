9e7fd3017440a4d98616470bb8d9f47db7bba464
Ricardo Luiz Junior <94785832+MrLous@users.noreply.github.com>
2023-12-20 09:51:53 -0300

    Update Presentation.js

--- src/components/section/Presentation.js
+++ src/components/section/Presentation.js
@@ -8,7 +8,7 @@ function Presentation(){
         <h1> Olá, me chamo Ricardo Luiz.</h1>
         <h6> mas pode me chamar de Juh</h6>
         <p>Sou universitário, cursando Engenharia da Computação pela Univesp<br/>
-        Comecei a trabalhar ainda como 15 anos na Industria metalúrgica, me tornei mais organizado e passei a planejar as tarefas conseguindo melhorar meus projetos e otimizando as entregas. Passei de menor aprendiz para auxiliar, estoquista, coordenador de estoque e comprador.<br/>
+        Comecei a trabalhar ainda com 15 anos na Industria metalúrgica, me tornei mais organizado e passei a planejar as tarefas conseguindo melhorar meus projetos e otimizando as entregas. Passei de menor aprendiz para auxiliar, estoquista, coordenador de estoque e comprador.<br/>
         Construí minha carreira melhorando e desenvolvendo, aplicativos e ferramentas para acelerar o processo fazendo uma gestão mais fiel com tomadas de decisões mais rápidas, essa evolução se deu através estudos, experiência e sim, erros.<br/>
         <br/>Atualmente atuo como designer e desenvolvedor autônomo, mas espero embreve estar fazendo parte do seu time</p>
         <ButtonA text='Conecte-se comigo!' link='https://www.linkedin.com/in/ricardoluizjr/'  target='_blank'/>
