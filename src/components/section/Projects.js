dbd04fcb4acfc0df36a0c24f388eb49e3244afae
MrLous <ricardoluizjr@live.com>
2024-10-15 21:27:49 -0300

    modified:   src/components/section/Projects.js 	modified:   src/components/section/Skills.module.css 	new file:   src/image/certificates/dataAny.png 	new file:   src/image/certificates/front.png 	new file:   src/image/certificates/fullstack.png 	new file:   src/image/certificates/games.png 	new file:   src/image/certificates/power-bi.png

--- src/components/section/Projects.js
+++ src/components/section/Projects.js
@@ -8,6 +8,10 @@ import piano from '../../image/projects/PIANO.PNG'
 import memory from '../../image/projects/memory.png'
 import ButtonBig from '../elements/ButtonBig'
 import terminal from '../../image/projects/terminal.png'
+import dataAny from '../../image/certificates/dataAny.png'
+import front from '../../image/certificates/front.png'
+import devgames from '../../image/certificates/games.png'
+import powerBi from '../../image/certificates/power-bi.png'
 
 
 function Projects(){
@@ -34,6 +38,7 @@ function Projects(){
         <div className={styles.selectCards}>
           <ButtonBig text="Projetos" functionClick={viewProject} style={styleProject} id="btnProjetos"/>
           <ButtonBig text="Games" functionClick={viewGame} style={styleGame} id="btnGames"/>
+          <ButtonBig text="Certificados" functionClick={viewCetificates} style={styleGame} id="btnGames"/>
         </div>
         { viewCard ==  "Projetos" &&
         ( <>
@@ -90,6 +95,43 @@ function Projects(){
           descripion = "Game para aliviar do stres do dia a dia."
           repo = 'https://github.com/MrLous/crud-task'
           site = "https://sprightly-bienenstitch-4aaaf2.netlify.app"
+          />
+          </>
+          )
+        }
+
+        {  viewCard ==  "Certificados" &&
+        ( <>
+          <Cards
+          img = {front}
+          title = "Front-End com React.js"
+          tech = "HTML, JS, CSS, FIGMA - REACT.js"
+          descripion = "Curso Oferecido pela escola DNC, com 40h, concluido DEZ/2023."
+          repo = 'black'
+          site = "https://ed.escoladnc.com/formacoes-g/?utm_source=adwords&utm_medium=paid&utm_campaign=hr-ga-geral-312-1&utm_content=hr-ga-geral-312-1-1&utm_term=dnc&utm_term=dnc&utm_campaign=%5B312%5D%5BOP%5D%5BHR%5D%5BGeral%5D%5BSearch%5D&utm_source=adwords&utm_medium=ppc&hsa_acc=1620955800&hsa_cam=21542018591&hsa_grp=165520193733&hsa_ad=708070867381&hsa_src=g&hsa_tgt=kwd-13228606&hsa_kw=dnc&hsa_mt=e&hsa_net=adwords&hsa_ver=3&gad_source=1&gclid=CjwKCAjwpbi4BhByEiwAMC8JnaiD7UCZ5hkR3JIZNTEGqbqESuXYBWI0-u-9_vwGGrvjVihKQkzRqhoCSEUQAvD_BwE"
+          />
+          <Cards
+          img = {devgames}
+          title = "Desenvolvedor de Jogos"
+          tech = "HTML, JS, CSS, iks, Godot"
+          descripion = "Curso Oferecido pela escola DIO, com 77h, concluido DEZ/2023"
+          repo = 'black'
+          site = "https://www.dio.me/"
+          /><Cards
+          img = {dataAny}
+          title = "Data Analytics"
+          tech = "SQL, R e Tableu"
+          descripion = "Curso Oferecido pela escola Coursera/Google, com 120h, concluido MAR/2023."
+          repo = 'black'
+          site = "https://www.coursera.org/courseraplus/?utm_medium=sem&utm_source=gg&utm_campaign=B2C_LATAM_coursera_FTCOF_courseraplus&campaignid=20844412474&adgroupid=159999536361&device=c&keyword=coursera&matchtype=b&network=g&devicemodel=&adposition=&creativeid=683667892738&hide_mobile_promo&gad_source=1&gclid=CjwKCAjwpbi4BhByEiwAMC8JndRXMNurKZ5rzi6_MuY-QhXaQG6Rpofo18cfEa0Qhuj1f6fWjWGfChoCYm4QAvD_BwE"
+          />
+           <Cards
+          img = {powerBi}
+          title = "Data Analytics com Power-Bi"
+          tech = "SQL, R, Python Power-Bi"
+          descripion = "Curso Oferecido pela escola Dio, com 82h, concluido SET/2024."
+          repo = 'black'
+          site = "https://www.dio.me/"
           />
           </>
           )
