a0b6b89b987e9866e8aea79ce7417652c2847028
MrLous <ricardoluizjr@live.com>
2024-10-15 22:19:24 -0300

    correção texto apresentação

--- src/components/section/Presentation.js
+++ src/components/section/Presentation.js
@@ -7,11 +7,11 @@ function Presentation(){
         <h4><strong> Bem-vindo ao meu Portifólio</strong></h4>
         <h1> Olá, me chamo Ricardo Luiz.</h1>
         <h6> mas pode me chamar de Juh</h6>
-        <p>Meu modo de atuação sempre foi em busca de melhorias num modo geral,<br/>
-        e tentando solucionar alguns problemas através da construção de planilhas, nasceu o gosto pela programação..<br/>
-        Ao passar do tempo, entendi que não apenas o programa precisa ser estruturado, mas o processo como um todo,<br/>
-        desde a primeira informação, ou seja, o problema a ser solucionado, e com isso, me aprofundei mais em Análises de dados.<br/>
-        <br/>Busco constante melhora,  e com isso aguardo uma oportunidade para essa evolução.</p>
+        <p>Meu modo de atuar sempre foi em busca de melhorias de um modo geral,<br/>
+         e tentando solucionar alguns problemas através da construção de planilhas, nasceu o gosto pela programação..<br/>
+        Ao passar do tempo, entendi que não apenas o programa precisa ser estruturado, mas o processo como um todo, desde a primeira informação,<br/>
+         ou seja, o problema a ser solucionado, e com isso, me aprofundei mais em Análises de dados e Desenvolvimento de Sistemas.<br/>
+        <br/>Busco constante melhorar e com isso aguardo uma oportunidade para essa evolução.</p>
         <ButtonA text='Conecte-se comigo!' link='https://www.linkedin.com/in/ricardoluizjr/'  target='_blank'/>
       </div> 
     )
