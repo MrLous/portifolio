886feca7ab31ad3162113fffd41fe089dd5d39a4
MrLous <ricardoluizjr@live.com>
2024-11-20 10:53:13 -0300

    import do curriculo

--- src/components/section/Footer.js
+++ src/components/section/Footer.js
@@ -1,11 +1,12 @@
 import { FaWhatsapp, FaLinkedin, FaGithub, FaRegFilePdf } from "react-icons/fa";
 import styles from './Footer.module.css'
+import curriculo from '../../pdf/Curriculo .pdf'
 
 function Footer(){
     return(
       <div className={styles.footer}>
         <ul>
-            <li className={styles.pdf}><a href='../../pdf/Curriculo .pdf' target='_blank'><FaRegFilePdf size={30}/></a></li>
+            <li className={styles.pdf}><a href={curriculo} target='_blank'><FaRegFilePdf size={30}/></a></li>
             <li className={styles.whatsapp}><a href='https://wa.me//551692925034' target='_blank'><FaWhatsapp size={30} /></a></li>
             <li className={styles.git}><a href='https://github.com/MrLous' target='_blank'><FaGithub size={30}/></a></li>
             <li className={styles.linkdin}><a href='https://www.linkedin.com/in/ricardoluizjr/' target='_blank'><FaLinkedin size={30}/></a></li>
